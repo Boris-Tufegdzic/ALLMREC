595ab59ac1fc6e1cc5988ebf1420aab08275f01d
Boris Tufegdzic <boris.tufegdzic@tum.de>
2024-11-24 16:13:36 +0100

    updated data filtering

--- data/yelp/filter_data.py
+++ data/yelp/filter_data.py
@@ -7,6 +7,7 @@ user_file = "yelp_academic_dataset_user.json"
 
 # Output file paths
 filtered_business_file = "philadelphia/philadelphia_businesses.json"
+half_filtered_review_file = "philadelphia/philadelphia_hf_reviews.json"
 filtered_review_file = "philadelphia/philadelphia_reviews.json"
 filtered_user_file = "philadelphia/philadelphia_users.json"
 
@@ -25,7 +26,7 @@ with open(business_file, "r", encoding="utf-8") as infile, open(filtered_busines
 # Step 2: Filter reviews of Philadelphia businesses
 philadelphia_user_ids = set()
 
-with open(review_file, "r", encoding="utf-8") as infile, open(filtered_review_file, "w", encoding="utf-8") as outfile:
+with open(review_file, "r", encoding="utf-8") as infile, open(half_filtered_review_file, "w", encoding="utf-8") as outfile:
     for line in infile:
         review = json.loads(line)
         if review["business_id"] in philadelphia_business_ids:
@@ -38,18 +39,29 @@ with open(review_file, "r", encoding="utf-8") as infile, open(filtered_review_fi
 user_threshold = 5
 # Read Philadelphia reviews and count user reviews in Philadelphia
 user_review_count = {}
-with open("philadelphia/philadelphia_reviews.json", "r", encoding="utf-8") as review_file:
+with open(half_filtered_review_file, "r", encoding="utf-8") as review_file:
     for line in review_file:
         review = json.loads(line)
         user_id = review["user_id"]
         user_review_count[user_id] = user_review_count.get(user_id, 0) + 1
 
-# Write filtered users to the new JSON file
-with open("philadelphia/philadelphia_users.json", "w", encoding="utf-8") as user_outfile:
-    with open("yelp_academic_dataset_user.json", "r", encoding="utf-8") as user_file:
+# U to the new JSON file
+filtered_user_ids = set()
+with open(filtered_user_file, "w", encoding="utf-8") as user_outfile:
+    with open(user_file, "r", encoding="utf-8") as user_file:
         for line in user_file:
             user = json.loads(line)
             if user_review_count.get(user["user_id"], 0) >= user_threshold:
                 user_outfile.write(json.dumps(user) + "\n")
+                filtered_user_ids.add(user["user_id"])
+
+# Filter reviews based on filtered_user_ids
+with open(half_filtered_review_file, "r", encoding="utf-8") as infile, open(filtered_review_file, "w", encoding="utf-8") as outfile:
+    for line in infile:
+        review = json.loads(line)
+        if review["user_id"] in filtered_user_ids:
+            json.dump(review, outfile)
+            outfile.write("\n")
+
 
 print("Filtered data has been saved successfully!")
